e65897a0e498c53b789a5185dfc6e4bfeb1b5a26
Ruz <ruz@example.com>
2025-12-06 18:19:37 +0200

    Refactor

--- whatsapp_bot/whatsapp/mass_messages/db.py
+++ whatsapp_bot/whatsapp/mass_messages/db.py
@@ -1,26 +1,39 @@
+from typing import Dict
 from sqlalchemy import text
 
+from api.base_models import ParticipantItem
+from job_and_listener.job.models.job_model import Job
 
-# def insert_participants_to_sql(cur, participants):
-#     """
-#     Inserts a list of participants into the mass_messages table.
-#     Skips any duplicates based on the primary key 'id'.
 
-#     Args:
-#         cur: Active DB cursor
-#         participants: List of participant objects with 'id' and 'phone_number' attributes
-#     """
-#     insert_sql = text("""
-#         INSERT INTO mass_messages (recipient_id, recipient_phone_number, success)
-#         VALUES (:recipient_id, :recipient_phone_number, :success)
-#     """)
+def insert_to_mass_messages_sql(
+    cur, batch_id: str, job_dict: Dict[ParticipantItem, "Job"]
+):
+    """
+    Inserts participants into mass_messages table with the corresponding job_info_id.
+    Assumes participants and job_list are in the same order.
 
-#     for part in participants:
-#         cur.execute(
-#             insert_sql,
-#             {
-#                 "id": part.id,
-#                 "phone_number": part.phone_number,
-#                 "success": None
-#             }
-#         )
+    Args:
+        cur: DB cursor
+        participants: list of participant objects with 'id' and 'phone_number'
+        batch_id: the batch ID for this insert
+        job_list: list of Job objects corresponding to participants
+    """
+    insert_sql = text("""
+        INSERT INTO mass_messages (
+            batch_id, recipient_id, recipient_phone_number, job_info_id
+        ) VALUES (
+            :batch_id, :recipient_id, :recipient_phone_number, :job_info_id
+        )
+    """)
+
+    for participant, job in job_dict.items():
+        cur.execute(
+            insert_sql,
+            {
+                "batch_id": batch_id,
+                "recipient_id": participant.id,
+                "recipient_phone_number": participant.phone_number,
+                "job_info_id": job.metadata.id,
+            }
+        )
+
