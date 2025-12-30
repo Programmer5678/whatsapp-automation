312604ea441c164156c3c08315b5164c74ae3990
Ruz <ruz@example.com>
2025-12-30 08:43:18 +0200

    Added CORS Policy ( allow all )

--- whatsapp_bot_backend/api/main.py
+++ whatsapp_bot_backend/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.group_creates import group_creates_router
 from routes.job import job_router
@@ -14,6 +14,13 @@ from api.setup.setup import setup
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,  # MUST be False when using "*"
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Register routers
 for router in [group_creates_router, job_router, test_router, participants_router, messages_router, connection_router ]:
     app.include_router(router)
